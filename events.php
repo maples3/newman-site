d35a0fa6a385b1fa8d8aa68dade9ed90ca52e58b
Anthony Mapes, Jr <anthonymapes3@gmail.com>
2017-08-08 07:32:36 -0400

    Updated events page for Welcome Week

--- events.php
+++ events.php
@@ -11,16 +11,42 @@
 <h1>Events</h1>
 
 <div class="story">
-<h3>Summer Events</h3>
-<p>We&apos;re going to be at the Summer Tailgate on July 27th, look for us in the Rec Center!
-  We will also be at FreshFusion (during the 3-day orientation), so make sure to stop by!</p>
-<p>On Thursday, August 24<sup>th</sup> (during the first week of classes) we will
-start of the school year by celebrating Mass in the UC Ballroom (second floor of the
-University Center, which is next to the bookstore) at 1:00pm.  Please join us in prayer as
-we ask God to bless the coming year.</p>
+<h3>Newman Welcome Week</h3>
+<p>This year, we've planned a Newman Welcome Week!  Every day during the first
+  week of classes, we're going to have something going on, so make sure to
+  drop by for at least some of them!
+  <ul>
+    <li><b>Monday, August 21</b>
+      <ul>
+        <li><a target="_blank" href="https://www.facebook.com/events/906410952849093/">Movie Night!</a></li>
+      </ul>
+    </li>
+    <li><b>Tuesday, August 22</b>
+      <ul>
+        <li><a target="_blank" href="https://www.facebook.com/events/211444406053654/">Game night!</a></li>
+      </ul>
+    </li>
+    <li><b>Wednesday, August 23</b>
+      <ul>
+        <li><a target="_blank" href="https://www.facebook.com/events/669918736551438">Newman at the Rec Center</a></li>
+      </ul>
+    </li>
+    <li><b>Thursday, August 24</b>
+      <ul>
+        <li><a target="_blank" href="https://www.facebook.com/events/122603408372913">Mass in the UC Ballroom</a></li>
+        <li><a target="_blank" href="https://www.facebook.com/events/835324989979351">Dinner &amp; Open House</a></li>
+      </ul>
+    </li>
+    <li><b>Friday, August 25</b>
+      <ul>
+        <li><a target="_blank" href="https://www.facebook.com/events/122289978405503">Luau Party!</a></li>
+      </ul>
+    </li>
+  </ul>
+</p>
 <p>You can join our
  <a href="https://www.facebook.com/groups/nkucatholicnewmancenter/" target="_blank">Facebook group</a>
-to stay up-to-date on what we have planned.</p>
+to stay up-to-date on all of our events, as well as general club discussions!</p>
 </div>
 
 <div class="story">
